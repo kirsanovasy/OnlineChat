99127644116acd9cb71b7ea1908a04b1f5a4fcdc
kirsanovasy <70444733+kirsanovasy@users.noreply.github.com>
2020-10-08 22:57:45 +0300

    Commit Message

---
+++ src/ru/geekbrains/OnlineChat/Server.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.OnlineChat;
+
+public class Server {
+        public void start(int port) {
+            System.out.println("Server started at: " + port);
+        }
+
+        public void stop() {
+            System.out.println("Server stopped");
+        }
+    }
+
